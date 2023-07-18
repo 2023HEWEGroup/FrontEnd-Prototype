834581c3e8c056f839ac142ee95d7fa5e9894907
hinata <atahin0607@gmail.com>
2023-07-18 10:30:31 +0900

    a

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { CssBaseline } from "@mui/material";
 import { lightBlueTheme } from "./layouts/theme";
 import Top from "./pages/Top";
 import Home from "./pages/Home";
-import Product from "./pages/product/Product";
+import Product from "./pages/Product";
 import Following from "./pages/Following";
 import Group from "./pages/Group";
 import Setting from "./pages/Setting";
@@ -13,7 +13,6 @@ import Notify from "./pages/Notify";
 import Profile from "./pages/Profile";
 import Help from "./pages/Help";
 import Info from "./pages/Info";
-import Trading from "./pages/product/trading/Trading";
 import CommonLayouts from "./layouts/CommonLayout";
 
 
@@ -26,9 +25,7 @@ function App() {
           <Route path="/" element={<Top />} />
           <Route path="/" element={<CommonLayouts />}>
             <Route path="home" element={<Home />} />
-            <Route path="product" element={<Product />}>
-              <Route path="trading" element={<Trading />} />
-            </Route>
+            <Route path="product" element={<Product />} />
             <Route path="following" element={<Following />} />
             <Route path="group" element={<Group />} />
             <Route path="exhibit" element={<Exhibit />} />
---
+++ src/layouts/product/myProduct/MyProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const MyProduct = () => {
+  return (
+    <></>
+  )
+}
+
+
+export default MyProduct
