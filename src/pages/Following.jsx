9b6e534d48a24d5cf8f0fab551ceba595afa701c
Show-525 <shoutashantian93@gmail.com>
2023-07-24 14:48:52 +0900

    アイコン追加

--- src/pages/Following.jsx
+++ src/pages/Following.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Box } from '@mui/material'
+import { Avatar } from '@mui/joy'
 
 const Following = () => {
   return (
-    <></>
+    <>
+    <Box>
+      <Avatar src = {"https://avatars.githubusercontent.com/u/85226500?v=4%22"} sx={{width:"150px",height:"150px"}}
+      />
+    </Box>
+    </>
   )
 }
 
