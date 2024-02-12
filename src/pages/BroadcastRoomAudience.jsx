aeaefcfa9810f6f18b7e112ef8b962c5e14e09b4
hinata <atahin0607@gmail.com>
2024-02-12 16:32:34 +0900

    ライブ配信ルーム準備完了

--- src/pages/BroadcastRoomAudience.jsx
+++ src/pages/BroadcastRoomAudience.jsx
@@ -6,31 +6,14 @@ import io from 'socket.io-client';
 const BroadcastRoomAudience = () => {
 
     const [socket, setSocket] = useState(null);
+    const [liverSocketId, setLiverSocketId] = useState(null);
+    const [peerConnection, setPeerConnection] = useState(null);
     const [isSockedIdUpdated, setIsSockedIdUpdated] = useState(false); // AudienceWindow展開時に参加者socketIdが更新されたらtureとなる。(1度しか呼ばないため)
     const { roomId } = useParams();
     const videoRef = useRef(null);
 
-    // 配信者から受け取ったオファーをRemote Descriptionとして設定する関数
-    // Remote Description = WebRTC における接続の相手側の SDP（Session Description Protocol）情報
-    const handleOffer = async (offer, liverSocketId) => {
-        try {
-            // Peer Connection を作成
-            const peerConnection = new RTCPeerConnection();
-            // 受け取ったオファーをRemote Descriptionとして設定
-            await peerConnection.setRemoteDescription(offer);
-            // Answerを生成 (相手側(この場合はここの参加者)からの返答)
-            const answer = await peerConnection.createAnswer();
-            // 生成したAnswerをLocal Descriptionとして設定 (Answerは逆にこちら側の自分の情報として保持、相手に返す。配信者はofferを持つ)
-            await peerConnection.setLocalDescription(answer);
-            // Answerを相手に送信
-            socket.emit('answerForLiver', answer, liverSocketId);
-        } catch (error) {
-            console.error('Error handling offer:', error);
-        }
-    };
-
     useEffect(() => {
-        if (socket) {
+        if (socket && peerConnection) {
             // windowが別だとsocketIdも別(別クライアントとして認識されるため)なので、Audience展開時に配信情報の参加者socketIDを更新する。(一度だけ実行)
             if (!isSockedIdUpdated) {
                 socket.emit(`updateAudienceSocketId`, roomId);
@@ -38,17 +21,67 @@ const BroadcastRoomAudience = () => {
             }
 
             // 配信者からのofferを受け取る
-            socket.on('offer', (offer, liverSocketId) => {
+            socket.on('offer', async (offer, liverSocketId) => {
+                setLiverSocketId(liverSocketId);
                 console.log("offer received!");
-                handleOffer(offer, liverSocketId);
+                try {
+                    // 受け取ったオファーをRemote Descriptionとして設定
+                    await peerConnection.setRemoteDescription(offer);
+                    // Answerを生成 (相手側(この場合はここの参加者)からの返答)
+                    const answer = await peerConnection.createAnswer();
+                    // 生成したAnswerをLocal Descriptionとして設定 (Answerは逆にこちら側の自分の情報として保持、相手に返す。配信者はofferを持つ)
+                    await peerConnection.setLocalDescription(answer);
+                    // Answerを相手に送信
+                    socket.emit('answerForLiver', answer, liverSocketId);
+                } catch (error) {
+                    console.error('Error handling offer:', error);
+                }
+            });
+
+            // 配信者からのICEを受け取る
+            socket.on('iceFromLiver', (ICE, liverSocketId) => {
+                console.log("ICE received!");
+                peerConnection.addIceCandidate(ICE);
             });
         }
-    }, [socket, roomId]); // eslint-disable-line react-hooks/exhaustive-deps
+        return () => {
+            // リスナーを解除
+            if (socket) {
+                socket.off('offer');
+                socket.off('iceFromLiver');
+            }
+        };
+    }, [socket, peerConnection]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    if (peerConnection) {
+        // 配信者にICE送信
+        peerConnection.onicecandidate = (e) => {
+            const ice = e.candidate;
+            socket.emit("iceFromAudience", ice, liverSocketId);
+        };
+    }
+
+    if (peerConnection) {
+        // 接続のステータス
+        peerConnection.onconnectionstatechange = () => {
+            console.log(peerConnection.connectionState);
+        };
+    }
+
+    if (peerConnection) {
+        peerConnection.ontrack = async (event) => {
+            console.log("ストリーム検知");
+            const stream = event.streams[0];
+            videoRef.current.srcObject = stream;
+        };
+    }
 
     useEffect(() => {
         // サーバーとの接続を確立
         const newSocket = io('http://localhost:5001');
+        const pc = new RTCPeerConnection();
         setSocket(newSocket);
+        setPeerConnection(pc);
         // クリーンアップ関数で接続を解除
         return () => {
             newSocket.disconnect();
--- src/pages/BroadcastRoomLiver.jsx
+++ src/pages/BroadcastRoomLiver.jsx
@@ -6,99 +6,94 @@ import io from 'socket.io-client';
 const BroadcastRoomLiver = () => {
 
     const [socket, setSocket] = useState(null);
+    const [audienceSocketId, setAudienceSocketId] = useState(null);
+    const [peerConnection, setPeerConnection] = useState(null);
     const [isSockedIdUpdated, setIsSockedIdUpdated] = useState(false); // LiverWindow展開時に配信者socketIdが更新されたらtureとなる。(1度しか呼ばないため)
     const { roomId } = useParams();
     const videoRef = useRef(null);
 
-    // useEffect(() => {
-    //     let peerConnection = null;
-    
-    //     const initializeOffer = async () => {
-    //         try {
-    //             // オファーの作成
-    //             const offer = await peerConnection.createOffer();
-    //             // オファーをセット
-    //             await peerConnection.setLocalDescription(offer);
-    //             // オファーをシグナリングサーバーを介して送信
-    //             socket.emit('offer', peerConnection.localDescription);
-    //         } catch (error) {
-    //             console.error('Error creating offer:', error);
-    //         }
-    //     };
-    
-    //     // メディアストリーム取得。
-    //     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    //     .then((stream) => {
-    //         // WebRTCセットアップを行う
-    //         peerConnection = new RTCPeerConnection();
-    //         stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-    //         initializeOffer();
-    //     })
-    //     .catch((error) => {
-    //         console.log(error);
-    //     });
-    
-    //     return () => {
-    //         if (peerConnection) {
-    //             peerConnection.close();
-    //         }
-    //     };
-    // }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
     useEffect(() => {
-        if (socket) {
+        if (socket && peerConnection) {
             // windowが別だとsocketIdも別(別クライアントとして認識されるため)なので、Liver展開時に配信情報の配信者socketIDを更新する。(一度だけ実行)
             if (!isSockedIdUpdated) {
                 socket.emit(`updateLiverSocketId`, roomId);
                 setIsSockedIdUpdated(true);
-            }
+            };
 
             // 配信者に新たな参加者が来たことを通知(offerを促すcallを受け取る)
-            socket.on('newAudience', (audienceSocketId) => {
+            socket.on('newAudience', async (audienceSocketId)  => {
+                setAudienceSocketId(audienceSocketId);
                 console.log(`New audience joined with socket ID: ${audienceSocketId}`);
-                // オファーを作成して送信する
-                const peerConnection = new RTCPeerConnection();
-                navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-                .then((stream) => {
-                    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-                    return peerConnection.createOffer();
-                })
-                .then((offer) => {
-                    return peerConnection.setLocalDescription(offer);
-                })
-                .then(() => {
-                    // 新しい参加者にオファーを送信する
-                    socket.emit('offerToNewAudience', audienceSocketId, peerConnection.localDescription);
-                })
-                .catch((error) => {
-                    console.error('Error creating and sending offer:', error);
-                });
+                // オファーを作成して新たな参加者に送信する
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+                stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+                const offer = await peerConnection.createOffer();
+                await peerConnection.setLocalDescription(offer);
+                socket.emit('offerToNewAudience', audienceSocketId, peerConnection.localDescription);
+            });
 
-                // 配信者からのアンサーを受け取る
-                socket.on('answer', (answer, audienceSocketId) => {
-                    console.log("Received answer from sender with socket ID:", audienceSocketId);
-                    // アンサーを自身のピア接続に設定
-                    peerConnection.setRemoteDescription(answer)
-                    .then(() => {
-                        console.log("Remote description set successfully.");
-                        // これ以降、ピア接続を介してデータの送受信が可能
-                    })
-                    .catch((error) => {
-                        console.error("Error setting remote description:", error);
-                    });
-                });
+            // 配信者からのアンサーを受け取る
+            socket.on('answer', async (answer, audienceSocketId) => {
+                console.log("Received answer from sender with socket ID:", audienceSocketId);
+                // アンサーを自身のピア接続に設定
+                await peerConnection.setRemoteDescription(answer);
+                console.log("Remote description set successfully.");
+                // これ以降、ピア接続を介してデータの送受信が可能
+            });
+
+            // 参加者からのICEを受け取る
+            socket.on('iceFromAudience', (ICE, audienceSocketId) => {
+                console.log("ICE received!");
+                peerConnection.addIceCandidate(ICE);
             });
         }
-    }, [socket, roomId]); // eslint-disable-line react-hooks/exhaustive-deps
+        return () => {
+            // リスナーを解除
+            if (socket) {
+                socket.off('newAudience');
+                socket.off('answer');
+                socket.off('iceFromAudience');
+            }
+        };
+    }, [socket, peerConnection]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    if (peerConnection) {
+        // 配信者にICE送信
+        peerConnection.onicecandidate = (e) => {
+            const ice = e.candidate;
+            socket.emit("iceFromPeer", ice, audienceSocketId);
+        };
+    }
+
+    if (peerConnection) {
+        // 接続のステータス
+        peerConnection.onconnectionstatechange = () => {
+            console.log(peerConnection.connectionState);
+        };
+    }
 
     useEffect(() => {
         // サーバーとの接続を確立
         const newSocket = io('http://localhost:5001');
+        const pc = new RTCPeerConnection();
         setSocket(newSocket);
+        setPeerConnection(pc);
         // クリーンアップ関数で接続を解除
         return () => {
             newSocket.disconnect();
         };
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    useEffect(() => {
+        const initMediaStream = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+                videoRef.current.srcObject = stream;
+            } catch (error) {
+                console.error('Error accessing media devices:', error);
+            }
+        };
+        initMediaStream();
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
